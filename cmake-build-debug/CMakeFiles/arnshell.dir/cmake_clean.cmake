8351ef872c64058a9d3f973150884d5b9c193d7e
Arnold Balliu <arnold.balliu@gmail.com>
2017-01-30 21:23:07 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/arnshell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arnshell.dir/main.cpp.o"
+  "arnshell.pdb"
+  "arnshell"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arnshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
